a6c600269a64f9115cd881011db3f09fe82a7f33
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-09-20 17:30:28 +0100

    creating a binary tree node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,11 +11,11 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *temp;
+	binary_tree_t *node;
 
 	node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 
-	if (temp == NULL)
+	if (node == NULL)
 		return (NULL);
 
 	node->n = value;
