a9c55f52fdee3cdd442f501c1ad9822ba0a92594
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-09-22 19:22:18 +0100

    function that goes through a binary tree using pre-order traversal

--- binary_trees.h
+++ binary_trees.h
@@ -20,6 +20,7 @@ typedef struct binary_tree_s
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
 } binary_tree_t;
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_root(const binary_tree_t *node);
 int binary_tree_is_leaf(const binary_tree_t *node);
 void binary_tree_delete(binary_tree_t *tree);
