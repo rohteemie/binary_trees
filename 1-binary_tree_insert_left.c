2f7415950105320a3c559a84aa75f08f4c4fc111
Rotimi Owolabi <iamrotimiowolabi@gmail.com>
2022-09-23 01:14:20 +0100

    updated to betty linter compliant

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -3,7 +3,7 @@
 
 /**
  * binary_tree_insert_left - Function inserts a node as a left-child
- * 			     of another in a binary tree.
+ * of another in a binary tree.
  * @parent: A pointer to the node to insert the left-child in.
  * @value: The value to store in the new node.
  * Return: If parent is NULL or an error occurs - NULL.
