e2b2027007252e8ba9d087899d11619db561453f
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-09-26 00:15:18 +0100

    nj

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,19 +12,19 @@ int binary_tree_balance(const binary_tree_t *tree)
 	int n;
 	size_t leftChild = 0;
 	size_t rightChild = 0;
-	if (tree)
+	if (!tree)
+		return (0);
+	if (tree->left)
 	{
-		if (tree->left)
-		{
-			leftChild += 1;
-			leftChild += binary_tree_balance(tree->left);
-		}
-		if (tree && tree->right)
-		{
-			rightChild += 1;
-			rightChild += binary_tree_balance(tree->right);
-		}
+		leftChild += 1;
+		leftChild += binary_tree_balance(tree->left);
 	}
-		n = leftChild - rightChild;
-		return (n);
+
+	if (tree->right)
+	{
+		rightChild += 1;
+		rightChild += binary_tree_balance(tree->right);
+	}
+	n = leftChild - rightChild;
+	return (n);
 }
