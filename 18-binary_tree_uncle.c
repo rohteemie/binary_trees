d5be241ee710591cefb453c64515518609fdb70d
Rotimi Owolabi <iamrotimiowolabi@gmail.com>
2022-09-26 08:57:25 +0100

    function to check uncle of a node in a BT

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+#include "17-binary_tree_sibling.c"
+
+/**
+ * binary_tree_uncle - function that finds the uncle of a node
+ * @node: is a pointer to the node to find the uncle
+ * Return: uncle of a node
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *uncle;
+
+	if (!node || !node->parent)
+		return (NULL);
+	uncle = binary_tree_sibling(node->parent);
+	return (uncle);
+}
