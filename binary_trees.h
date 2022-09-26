2afda5b8687351c048c6c0db9d5b8c283273ad14
Rotimi Owolabi <iamrotimiowolabi@gmail.com>
2022-09-26 08:54:17 +0100

    function to check siblings of a node in a BT

--- binary_trees.h
+++ binary_trees.h
@@ -38,6 +38,7 @@ void binary_tree_print(const binary_tree_t *);
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 
 
 
